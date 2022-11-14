2ba0dcf24da035af6039b8b72a867002d1132381
longthb3 <tranhoangbaolong0201@gmail.com>
2022-11-14 17:43:11 +0700

    fix bug

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import App from './App.vue';
 import 'ant-design-vue/es/style/default.less';
 import './assets/styles/app.scss';
 
+localStorage.setItem('user', JSON.stringify({ username: 'long', passwd: '1234' }));
+
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
